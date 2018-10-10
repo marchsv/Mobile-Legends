f6ccf9d5608e51754e6e8c03947948b0ee685a75
rogermch <mrchsvnt@gmail.com>
2018-10-10 13:34:38 +0700

    \

---
+++ contoh1.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>MOBILE LEGENS</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body bgcolor="#f4f4f4">
+<div id ="container"> 
+
+	<div class="Header">
+		<div id="navigation">
+			<left>
+				<tr>
+					<td> <a href="">HOME</a> </td>
+					<td> <a href=""><u>NEWS</u></a> </td>
+					<td> <a href="">HEROES</a> </td>
+					<td> <a href="">HOT IN 2018</a> </td>
+				</tr>
+			</left>
+		</div>
+		<div class="button">
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br><br>
+			<br>
+			<br><br>
+			<br>
+			<br><br>
+			<br>
+			<br><br>
+			<br>
+			<br><br>
+			<br>
+			<br>
+		</div>			
+	</div>
+	
+	<div id="about">
+		<div class="judul">
+			HEROES
+		</div>
+		<div class="matris">
+			HOW TO MARTIS:<br><br>
+		</div>
+		<div class="gmatris">
+			<img src="martis.png">
+		</div>
+		<div class ="right"> 	
+			1. Gunakan combo serangan:<br>
+				<l>Skill 1 > Skill 2 > Basic Attack > Skill 2 > Skill 3<br></l>
+				<l>Skill 2 > Basic Attack > Skill 2 > Skill 1 > Basic Attack > Skill 3<br></l>
+			2. Jangan ultimate musuh sebelm HP mereka di bawah 50%.<br>
+			3. Perbanyak aktivitas Jungling saat babak awal.<br>
+			4. Tunggu sampai lawan mengeluarkan skill dahulu<br>
+			5. pilih items Haas's Claws & Endless Battle
+		</div>	
+		<div class="alucard">
+			HOW TO ALUCARD:<br><br>
+		</div>
+		<div class="galucard">
+			<img src="alucard.png">
+		</div>
+		<div class ="right"> 	
+			1. Bergerak ke lane kosong saat jungle habis <br>
+			2. Jangan melawan musuh dengan lvl lebih besar sendirian<br>
+			3. gunakan kombinasi skill yang tepat:<br>
+			<l>Ground Splitter > Fission Wave > Whirling Smash > Fission Wave<br></l>
+			4. Jangan terobsesi mengincar kill <br>
+			5. Incar hero musuh yang paling empuk
+		</div>	
+	</div>
+
+	
+	<div id="footer">
+		<z>Copyright © 2018 ANGGORO SUGIHONO</z>
+	</div>
+</div>
+</body>
+</html>
